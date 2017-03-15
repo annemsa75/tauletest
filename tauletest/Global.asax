db11f17eb975a884cb9962558ca07c8c10b00cde
Anne-M Sæther <anne.saether@gmail.com>
2017-03-15 19:02:55 +0100

    Add project files.

---
+++ tauletest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tauletest.MvcApplication" Language="C#" %>
---
+++ tauletest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tauletest.Startup))]
+namespace tauletest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
